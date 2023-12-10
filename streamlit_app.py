e6f318579afda0f555a5f0dff6d094701eb607b3
Aman Singh <151084522+AmanSingh11b@users.noreply.github.com>
2023-12-10 17:08:11 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,11 +1,10 @@
 import streamlit as st
-from streamlit.components.v1 import components
 
 def main():
     st.title("Streamlit + Kivy Facial Verification App")
 
-    # Embed the Kivy app as an iframe
-    components.iframe("http://localhost:8080", height=600, width=400, scrolling=True)
+    # Embed the Kivy app as an iframe using HTML
+    st.markdown('<iframe src="http://localhost:8080" width=400 height=600 style="border:none;"></iframe>', unsafe_allow_html=True)
 
 if __name__ == "__main__":
     main()
