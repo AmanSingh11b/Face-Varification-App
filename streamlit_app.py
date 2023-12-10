ad80aeaf8b297bff3721f023aed0c30a63d593e8
Aman Singh <151084522+AmanSingh11b@users.noreply.github.com>
2023-12-10 16:59:02 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,20 +1,10 @@
 import streamlit as st
-from streamlit.components.v1 import html as components_html
-
-class MyKivyApp(components_html.BaseComponent):
-    def __init__(self):
-        super().__init__(key="my_kivy_app")
-
-    def render(self, **kwargs):
-        from faceApp import CamApp  # Assuming your faceApp.py file contains the CamApp class
-        CamApp().run()
 
 def main():
     st.title("Streamlit + Kivy Facial Verification App")
 
-    # Embed the Kivy app as a Streamlit component
-    kivy_component = MyKivyApp()
-    kivy_component
+    # Embed the Kivy app as an iframe
+    st.frame("kivy_frame", src="http://localhost:8080", height=600, width=400)
 
 if __name__ == "__main__":
     main()
