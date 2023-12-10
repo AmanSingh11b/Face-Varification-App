acfa7b11c97bc16b70c1ef29856ebb96d2fcd0f9
Aman <pashkarsingh11b@gmail.com>
2023-12-10 15:54:21 +0530

    Add Streamlit app script

---
+++ streamlit_app.py
@@ -0,0 +1,25 @@
+import streamlit as st
+from kivy.app import App
+import numpy as np
+from PIL import Image as PILImage
+
+# Import your Kivy app
+from faceApp import CamApp
+
+def run_kivy_app():
+    app = CamApp()  # Create an instance of your Kivy app
+    app.run()
+
+def main():
+    st.title("Face Verification Streamlit App")  # Set the title of your Streamlit app
+
+    # Add any Streamlit components or customizations here
+
+    st.write("This is a Streamlit app that embeds a Kivy application.")
+    st.write("The Kivy app is running below:")
+
+    # Run your Kivy app within Streamlit
+    run_kivy_app()
+
+if __name__ == '__main__':
+    main()
